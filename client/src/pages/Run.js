4883ea33517fb53ac9d8221ebb4f729bc177d4cf
Claire Phillips <claireacam@gmail.com>
2021-06-19 13:15:07 -0400

    Added a run page.

---
+++ client/src/pages/Run.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+
+function Run() {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>Run</h1>
+            </Jumbotron>
+        </div>
+    );
+}
+
+export default Run;
