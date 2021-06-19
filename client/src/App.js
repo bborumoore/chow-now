d28f133eacc2540b00c056c764b8949343d59513
Claire Phillips <claireacam@gmail.com>
2021-06-19 13:17:56 -0400

    Updated app routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
+import NewRun from "./pages/NewRun";
+import Run from "./pages/Run";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import NoMatch from "./pages/NoMatch";
@@ -11,13 +14,16 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path={["/:id", "/dashboard:id"]}>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/dashboard/:id">
             <Dashboard />
           </Route>
           <Route exact path="/newrun">
             <NewRun />
           </Route>
-          <Route exact path="/run:id">
+          <Route exact path="/run/:id">
             <Run />
           </Route>
           <Route exact path="/login">
