514849d21cf5524356f8cffa49a8389dffe6dd0b
Claire Phillips <claireacam@gmail.com>
2021-06-18 20:35:59 -0400

    Created skeleton component for Dashboard page.

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+
+function Dashboard() {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>My Dashboard</h1>
+            </Jumbotron>
+        </div>
+    );
+}
+
+export default Dashboard;
