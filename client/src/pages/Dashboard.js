473ac3bfc68cc6706d74dbbc7ac83d0f73a497af
Claire Phillips <claireacam@gmail.com>
2021-06-19 13:30:02 -0400

    Dashboard and run pages now grab ids from the url.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,10 @@ import React from "react";
 import Jumbotron from "../components/Jumbotron";
 
 function Dashboard() {
+
+    // Grab id passed in url
+    const {id} = useParams();
+
     return (
         <div>
             <Jumbotron>
--- client/src/pages/Run.js
+++ client/src/pages/Run.js
@@ -2,6 +2,10 @@ import React from "react";
 import Jumbotron from "../components/Jumbotron";
 
 function Run() {
+
+    // Grab id passed in url
+    const {id} = useParams();
+
     return (
         <div>
             <Jumbotron>
