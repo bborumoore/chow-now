22925cbef90ba00a19237c693db6a6284fd99d25
Marina Russ <marina@cameystudios.com>
2021-06-26 11:40:30 -0400

    adds meal components

---
+++ client/src/components/Meal/MealBox/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+// MealBox displays meal name, MealItem child components, and total meal cost
+
+// TODO: DB needs orderMealName, orderTotalPrice
+
+function MealBox({ children }) {
+  return (
+    <div style={{ textAlign: "center" }} className="mealbox">
+      <h2>#6 Deluxe{props.orderMealName}</h2>
+      {children}
+
+      <h2 className="order-total-price">$9.25 ${props.orderTotalPrice}</h2>
+    </div>
+  );
+}
+
+export default MealBox;
---
+++ client/src/components/Meal/MealEdit/MealEditItem/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+// TODO: is input's prop done correctly?
+
+function MealItemEdit(props) {
+  return (
+    <div className="row add-item-row">
+      <div className="item-info">
+        <div className="row">
+          <div className="col-1 item-icon add">
+            <i>❌</i>
+          </div>
+          <div className="col-7 item-name add">
+            Sprite {props.orderItemName}
+            <input type="text" placeholder="Add additional notes here" />{" "}
+            {props.orderItemNotes}
+          </div>
+
+          <div className="col-1 item-qty">x{props.orderItemQty}</div>
+          <div className="col-2 item-price">${props.orderItemPrice}</div>
+        </div>
+      </div>
+
+      <div className="btn-item-add">
+        <div className="row">
+          <div className="col-7">&nbsp;</div>
+          <div className="col-4">
+            <button className="btn-add-item">+ Add Item</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MealItemEdit;
