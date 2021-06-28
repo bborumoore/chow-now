633b92085c578c09381de99d2f636a860241fa2b
Marina Russ <marina@cameystudios.com>
2021-06-28 14:36:04 -0400

    creates add meal page

---
+++ client/src/pages/AddMeal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+import Meal from "../components/Meal";
+import MealBox from "../components/Meal/MealBox";
+import MealItem from "../components/Meal/MealItem";
+// import MealEdit from "../components/Meal/MealEdit";
+import { useParams } from "react-router-dom";
+
+function AddMeal() {
+
+    // JavaScript goes here
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default AddMeal;
