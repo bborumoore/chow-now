39019c48df1ca995fd113e0fe09a58efa4328e59
Claire Phillips <claireacam@gmail.com>
2021-06-30 20:45:26 -0400

    Created an example Run page, javascript for filling in real info still needs to be done

--- client/src/pages/Run.js
+++ client/src/pages/Run.js
@@ -1,17 +1,24 @@
 import React from "react";
-import Jumbotron from "../components/Jumbotron";
 import { useParams } from "react-router-dom";
+import RestaurantBox from "../components/RestaurantBox";
+import StatusBar from '../components/StatusBar';
 
 function Run() {
 
     // Grab id passed in url
     const {id} = useParams();
 
+    // Create a list of components with the users (invitees) and their orders.
+    let invitees = [<div>Bryce</div>,
+                    <div>Thomas</div>]; // Placeholder info for now
+
     return (
         <div>
-            <Jumbotron>
-                <h1>Run</h1>
-            </Jumbotron>
+            <p>This is all placeholder info for now</p>
+            <RestaurantBox restaurant_name="Imaginary Restaurant" address="1234 5th st."/>
+            <StatusBar status="started" time="12:30 PM"/>
+            <h3>Group:</h3>
+            {invitees}
         </div>
     );
 }
