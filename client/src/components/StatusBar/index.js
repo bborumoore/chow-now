120c22f38171d156ca50555221515da79181729c
Claire Phillips <claireacam@gmail.com>
2021-06-30 20:44:37 -0400

    Reworked status bar (could not figure out Grid components, so used straight bootstrap)

--- client/src/components/StatusBar/index.js
+++ client/src/components/StatusBar/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "../Grid";
 import "./style.css";
 
 // Run page will hold one of these components and will need to use state to update the props when they change on Run pages end.
@@ -8,42 +7,42 @@ function StatusBar(props) {
     let status;
     let time = props.time; // Formatting will need to be done here once we know how we are passing the time
     switch(props.status){
-        case "taking":
+        case "started":
             status = "Taking Orders";
             time = "Place by " + time;
-            bar =   <Row>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "white"}}></Col>
-                        <Col style={{"background-color": "white"}}></Col>
-                        <Col style={{"background-color": "white"}}></Col>
-                    </Row>;
+            bar =   <div className="row">
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "white"}}></div>
+                        <div className="col-3" style={{"background-color": "white"}}></div>
+                        <div className="col-3" style={{"background-color": "white"}}></div>
+                    </div>;
             break;
-        case "placed":
+        case "ordered":
             status = "Order Placed";
-            bar =   <Row>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "white"}}></Col>
-                        <Col style={{"background-color": "white"}}></Col>
-                    </Row>;
+            bar =   <div className="row">
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "white"}}></div>
+                        <div className="col-3" style={{"background-color": "white"}}></div>
+                    </div>;
             break;
         case "pickedUp":
             status = "Food Picked Up";
-            bar =   <Row>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "white"}}></Col>
-                    </Row>;
+            bar =   <div className="row">
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "white"}}></div>
+                    </div>;
             break;
         case "delivered":
             status = "Food Delivered";
-            bar =   <Row>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                        <Col style={{"background-color": "dark-gray"}}></Col>
-                    </Row>;
+            bar =   <div className="row">
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                        <div className="col-3" style={{"background-color": "gray"}}></div>
+                    </div>;
             break;
         default:
             throw 'No status given to StatusBar component!';
@@ -51,12 +50,12 @@ function StatusBar(props) {
 
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col>{status+' - '+ time}</Col>
-                </Row>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">{status+' - '+ time}</div>
+                </div>
                 {bar}
-            </Container>
+            </div>
         </div>
     );
 }
