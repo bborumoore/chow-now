e4ad3f176a100ea2f297d036363a2bd129c5811d
bborumoore <bborumoore@gmail.com>
2021-07-01 20:55:09 -0400

    Commented out express-session

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const session = require('express-session');
+// const session = require('express-session');
 // const MongoStore = require('connect-mongo')(express);
 const mongoose = require("mongoose");
 const routes = require("./routes");
