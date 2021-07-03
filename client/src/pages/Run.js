e16e15e5dd80e7388178ef534247d3c463f48091
Claire Phillips <claireacam@gmail.com>
2021-07-02 20:51:19 -0400

    Added several buttons to Run page
    1. Added a button for getting the run's url, which can be used to invite friends to the run.
    2. Added a "Add me to run!" button for users that do not have orders yet. (this is an h3 placeholder for now)
    3. Added a "Place Order" button for the runner. (this is an h3 placeholder for now)

--- client/src/pages/Run.js
+++ client/src/pages/Run.js
@@ -4,19 +4,23 @@ import RestaurantBox from "../components/RestaurantBox";
 import UserOrder from "../components/UserOrder";
 import StatusBar from "../components/StatusBar";
 import API from "../utils/API.js";
+import { getFromStorage } from "../utils/storage";
 
-async function getRunFromAPI(rid, restNameCB, restAddressCB, statusCB, timeCB, ordersCB) {
+async function getRunFromAPI(rid, token, restNameCB, restAddressCB, statusCB, timeCB, ordersCB, inRunCB, isRunnerCB) {
     await API.getRun(rid)
         .then((res)=>{
+            if(res.data.runner === token)
+                isRunnerCB(true);
+
             restNameCB(res.data.restaurantName);
             restAddressCB(res.data.restaurantAddress);
             statusCB(res.data.status);
-            getOrdersFromAPI(res.data,ordersCB);
+            getOrdersFromAPI(res.data,ordersCB, token, inRunCB);
         })
         .catch(err => console.log(err));
 }
 
-async function getOrdersFromAPI(run, orderCB) {
+async function getOrdersFromAPI(run, orderCB, token, inRunCB) {
     const orders_bare = run.orders;
     const order_ids = [];
     for (let i = 0; i < orders_bare.length; i++)
@@ -29,21 +33,38 @@ async function getOrdersFromAPI(run, orderCB) {
         .then((res) => res.data)
         .then(async (data) => {
             const orders_user = data.user;
+            if( orders_user === token )
+                inRunCB(true);
             const order_total = (data.orderTotal/100).toFixed(2);
             await API.getUser(orders_user)
             .then((res) => {
                 const user_name = res.data.firstName;
-                orders.push(<UserOrder name={user_name} total={order_total} status={"*need status in order model*"} />);
+                orders.push(<UserOrder key={user_name} name={user_name} total={order_total} status={"*need status in order model*"} />);
             });
         });
     }
     orderCB(orders);
 }
 
+function copyToClipboard(event) {
+    event.preventDefault();
+    const copyText = document.getElementById("inviteLink").getAttribute('href');
+
+    document.addEventListener('copy', function(event) {
+        event.clipboardData.setData('text/plain', copyText);
+        event.preventDefault();
+    }, true);
+
+    document.execCommand('copy');  
+    console.log('copied text : ', copyText);
+    alert('copied text: ' + copyText); 
+}
+
 function Run() {
 
     // Grab run id passed in url
-    const {rid} = useParams();
+    const {id} = useParams();
+    const { token } = getFromStorage('chow-now');
 
     // Get all runs
     const [restaurant_name, setRestName] = useState("");
@@ -51,17 +72,29 @@ function Run() {
     const [status, setStatus] = useState("started");
     const [time, setTime] = useState("*need time component in run model*");
     const [orders, setOrders] = useState([]);
+    const [userIsInRun, setUserIsInRun] = useState(false);
+    const [isRunner, setIsRunner] = useState(false);
     useEffect(() => {
-        getRunFromAPI(rid, setRestName, setRestAddress, setStatus, setTime, setOrders);
+        getRunFromAPI(id, token, setRestName, setRestAddress, setStatus, setTime, setOrders, setUserIsInRun, setIsRunner);
     }, []);
-    console.log(orders);
 
     return (
         <div>
-            <RestaurantBox restaurant_name={restaurant_name} address={restaurant_address} run_id={rid}/>
+            <RestaurantBox restaurant_name={restaurant_name} address={restaurant_address} run_id={id}/>
             <StatusBar status={status} time={time}/>
+            <h3><a  id="inviteLink" 
+                href={window.location.hostname + ":" + window.location.port + "/run/" + id} 
+                onClick={copyToClipboard}
+                style={{"color": "#000"}}>
+                    Click here for invite link!
+            </a></h3>
             <h3>Group:</h3>
             {orders}
+
+            <h3>Meal Placeholder</h3>
+
+            {!userIsInRun ? <h3>Add Me To Run!</h3> : false}
+            {isRunner ? <h3>Place Order</h3> : false}
         </div>
     );
 }
