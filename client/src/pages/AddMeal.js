82efb31b096f8f922f35d721fe614e2f2f327c2b
Marina Russ <marina@cameystudios.com>
2021-07-07 21:19:29 -0400

    comments out dummy data

--- client/src/pages/AddMeal.js
+++ client/src/pages/AddMeal.js
@@ -12,25 +12,25 @@ import MealEditBox from "../components/Meal/MealEdit/MealEditBox";
 function AddMeal() {
 
     const [listItems, setItems] = useState([
-        // DUMMY DATA FOR TESTING:
-        {
-            orderItemName: "Burger",
-            orderItemNotes: "extra cheese",
-            orderItemQty: "2",
-            orderItemPrice: "$3.25"
-        },
-        {
-            orderItemName: "Fries",
-            orderItemNotes: "",
-            orderItemQty: "1",
-            orderItemPrice: "$1.75"
-        },
-        {
-            orderItemName: "Drink",
-            orderItemNotes: "light ice",
-            orderItemQty: "1",
-            orderItemPrice: "$1.75"
-        }
+        // // DUMMY DATA FOR TESTING:
+        // {
+        //     orderItemName: "Burger",
+        //     orderItemNotes: "extra cheese",
+        //     orderItemQty: "2",
+        //     orderItemPrice: "$3.25"
+        // },
+        // {
+        //     orderItemName: "Fries",
+        //     orderItemNotes: "",
+        //     orderItemQty: "1",
+        //     orderItemPrice: "$1.75"
+        // },
+        // {
+        //     orderItemName: "Drink",
+        //     orderItemNotes: "light ice",
+        //     orderItemQty: "1",
+        //     orderItemPrice: "$1.75"
+        // }
     ]);
     useEffect(() => {
         console.log("test string for useEffect");
@@ -45,10 +45,10 @@ function AddMeal() {
                 <h1>Add Meal</h1>
             </Jumbotron>
             <MealBox listOfItems={listItems} />
-            <MealEditBox>
+            {/* <MealEditBox>
                 <MealEditItemHeader />
                 <MealEditItem />
-            </MealEditBox>
+            </MealEditBox>*/}
             <AddMealBtn />
         </div>
     );
